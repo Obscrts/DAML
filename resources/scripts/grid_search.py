a3eece45f1067a76a88640bfa82a332fba2f0bab
Obscrts <cedric.kloepsch@proton.me>
2024-09-22 01:54:15 +0200

    added typee ignore annotations

--- resources/scripts/grid_search.py
+++ resources/scripts/grid_search.py
@@ -9,21 +9,21 @@ param_grid = {
 }
 
 # GridSearchCV mit 5-facher Kreuzvalidierung
-grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=1)
+grid_search = GridSearchCV(estimator=rf_model, param_grid=param_grid, cv=5, n_jobs=-1, verbose=1) # type: ignore
 
 # Fitting des Modells mit den verschiedenen Hyperparameter-Kombinationen
-grid_search.fit(X_train_scaled, y_train)
+grid_search.fit(X_train_scaled, y_train) # type: ignore
 
 # Beste Parameter anzeigen
 best_params = grid_search.best_params_
 print(f'\nBeste Parameter: {best_params}')
 
 # Modell mit den besten Parametern auf den Testdaten evaluieren
-y_pred_best_rf = grid_search.best_estimator_.predict(X_test_scaled)
+y_pred_best_rf = grid_search.best_estimator_.predict(X_test_scaled) # type: ignore
 
 # Berechnung des Mean Squared Error (MSE) und des R²-Werts für das optimierte Modell
-mse_best_rf = mean_squared_error(y_test, y_pred_best_rf)
-r2_best_rf = r2_score(y_test, y_pred_best_rf)
+mse_best_rf = mean_squared_error(y_test, y_pred_best_rf) # type: ignore
+r2_best_rf = r2_score(y_test, y_pred_best_rf) # type: ignore
 
 # Ergebnisse anzeigen
 print(f'\nOptimiertes Modell - Mean Squared Error: {mse_best_rf}')
